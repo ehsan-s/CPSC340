6f11ac9d781d7a8c5f59f434525697cded64f432
ehsan <soltanaghaei97@gmail.com>
2021-10-01 16:09:00 -0700

    Random Forest Model

--- a2/code/random_tree.py
+++ a2/code/random_tree.py
@@ -1,7 +1,7 @@
 from random_stump import RandomStumpInfoGain
 from decision_tree import DecisionTree
+from scipy import stats
 import numpy as np
-
 import utils
 
 
@@ -19,9 +19,37 @@ class RandomTree(DecisionTree):
 
 
 class RandomForest:
+    num_trees = None
+    max_depth = None
+    random_trees = None
+
     """
     YOUR CODE HERE FOR Q4
     Hint: start with the constructor __init__(), which takes the hyperparameters.
     Hint: you can instantiate objects inside fit().
     Make sure predict() is able to handle multiple examples.
     """
+    def __init__(self, num_trees, max_depth):
+        self.num_trees = num_trees
+        self.max_depth = max_depth
+
+    def fit(self, X, y):
+        random_trees = np.empty(self.num_trees, dtype=object)
+        for i in range(self.num_trees):
+            random_tree = RandomTree(self.max_depth)
+            random_tree.fit(X, y)
+            random_trees[i] = random_tree
+
+        self.random_trees = random_trees
+
+    def predict(self, X):
+        n, d = X.shape
+        y_trees = np.zeros((self.num_trees, n))
+        y = np.zeros(n)
+        for i in range(self.num_trees):
+            y_trees[i, :] = self.random_trees[i].predict(X)
+
+        for i in range(n):
+            y[i] = stats.mode(y_trees[:, i].flatten())[0][0]
+
+        return y
